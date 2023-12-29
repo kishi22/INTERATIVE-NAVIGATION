850a4ae6af7aeb39af16b63fd952add47915cc08
Kishori Balasaheb Jadhav <141227032+kishi22@users.noreply.github.com>
2023-12-29 12:33:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamb = document. querySelector(".hamb") :
+const navlist = document.querySelector(".manu-overlay") :
+
+hamb.addeventlistener("click".function ()  {
+    navlist.classList.toggle("active") :
+    hamb.classList.toggle("click")  :
+})
